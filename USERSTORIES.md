c224de7d5a2b69ad431a479d3b53f1c798ced42a
maktubhelou <markallanevans@gmail.com>
2018-05-11 20:00:05 +0700

    added charge function following tutorial

--- USERSTORIES.md
+++ USERSTORIES.md
@@ -5,7 +5,10 @@
 Time spent: **X** hours spent in total
 ## Log
 + 11/05/2018 11:30am -> 12:20 ==> 50 min ;
-+ 11/05/2018 14:50pm -> 
++ 11/05/2018 14:50pm -> 15:30 ==> 40 min ;
++ 11/05/2018 19:00pm -> 20:00 ==> 60 min ;
+
+total = 150 min (2.5 hours);git remote add origin https://github.com/maktubhelou/swipe-central.git
 
 ## User Stories
 
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,9 +1,72 @@
 import React, { Component } from 'react';
+import { publicKey, secretKey } from '../keys/stripekeys';
 
 export default class Checkout extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+      chargeStatus: 'none',
+      charge: 200,
+    }
+    this.createCharge = this.createCharge.bind(this);
+  }
+
+  changeValue(value) {
+    this.setState({
+      charge: value
+    })
+  }
+
+  async createCharge() {
+    this.setState({
+      chargeStatus: 'Getting token'
+    })
+
+    const response = await fetch('https://api.stripe.com/v1/tokens', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${publicKey}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'card[number]=4242424242424242&card[exp_month]=02&card[exp_year]=2019'
+    })
+    const tokenData = await response.json();
+
+    this.setState({
+      chargeStatus: 'Charging card...'
+    });
+
+    const chargeResponse = await fetch('https://api.stripe.com/v1/charges', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${secretKey}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `amount=${this.state.charge * 100}&currency=usd&description=text_charges&source=${tokenData.id}`
+    })
+    const chargeData = await chargeResponse.json();
+    console.log(chargeData);
+    this.setState({
+      chargeStatus: chargeData.id
+    })
+  }
+
   render() {
+    console.log(publicKey);
+    console.log(secretKey);
     return (
-      <h1>Checkoutinternal</h1>
+      <div>
+        <h1>{this.props.name}</h1>
+        <div>
+        <input type="text" defaultValue={this.state.charge} onChange={(e) => this.changeValue(e.target.value)}></input>
+        </div>
+        <div>
+        <button onClick={this.createCharge} >Submit {this.state.charge}$ payment</button>
+        </div>
+        <h2>Current charge status: {this.state.chargeStatus}</h2>
+      </div>
     )
   }
 };
